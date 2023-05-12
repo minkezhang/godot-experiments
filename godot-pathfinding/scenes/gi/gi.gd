792c7c8f23f0ca5c5234b076f4ac577b54682ac1
Minke Zhang <minke.zhang@gmail.com>
2023-05-12 01:09:51 -0700

    Path updates

--- godot-pathfinding/scenes/gi/gi.gd
+++ godot-pathfinding/scenes/gi/gi.gd
@@ -1,6 +1,9 @@
 
 extends CharacterBody2D
 
+func _ready():
+	motion_mode = MOTION_MODE_FLOATING
+	$"NavigationAgent2D".path_desired_distance = 100
 """
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
--- godot-pathfinding/scenes/world/world.gd
+++ godot-pathfinding/scenes/world/world.gd
@@ -1,17 +1,28 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Map/GI/NavigationAgent2D.target_position = $Map/Base.map_to_local(Vector2i(11, 5))
-	print($Map/GI/NavigationAgent2D.is_target_reachable())
-	print($Map/GI/NavigationAgent2D.distance_to_target())
-	print()
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		$Map/GI/NavigationAgent2D.target_position = $Map/Base.map_to_local(
+			$Map/Base.local_to_map(
+				$Map/Base.to_local(get_global_mouse_position())
+			)
+		)
 	
-func _physics_process(_delta):
-	print($Map/GI/NavigationAgent2D.get_next_path_position())
+func _physics_process(delta):
+	var tile_position = $Map/Base.map_to_local(
+		$Map/Base.local_to_map(
+			$Map/Base.to_local($Map/GI/NavigationAgent2D.get_next_path_position())
+		)
+	)
+	var speed = 30
+	var velocity = (tile_position - $Map/GI.position).normalized() * speed
+	
+	# Reduce end-state jitter.
+	if (velocity * delta).length() >= (tile_position - $Map/GI.position).length():
+		$Map/GI.position = tile_position
+		print("JITTER")
+	else:
+		$Map/GI.set_velocity(velocity)
+		$Map/GI.move_and_slide()
