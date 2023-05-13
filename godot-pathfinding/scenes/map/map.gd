692ec001d966e9bc892cd92b0e37ccf3c2173d7b
Minke Zhang <minke.zhang@gmail.com>
2023-05-12 20:41:25 -0700

    Add AStar3D pathfinding implementation

--- godot-pathfinding/scenes/map/map.gd
+++ godot-pathfinding/scenes/map/map.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 const _VOID_ATLAS_COORDS = Vector2i(0, 0)
 var _astargrid: AStarGrid2D
+var _astar: AStar3D
 
 var _NULL_POS = Vector2i(-1, -1)
 var source = _NULL_POS
 var target = _NULL_POS
+
+var _ATLAS_GRASS = Vector2i(2, 0)
+
+var _dim: Vector3i
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -25,14 +30,35 @@ func _process(_delta):
 			target = tile
 			$Highlight.set_cell(0, target, 0, Vector2i(2, 0))
 	
+		print(source, target)
 		if source != _NULL_POS and target != _NULL_POS:
+			print("pathfind source: %s (%s) to target: %s (%s)" % [
+				source,
+				pack(Vector3i(source.x, source.y, 0), _dim),
+				target,
+				pack(Vector3i(target.x, target.y, 0), _dim)])
 			for n in $Highlight.get_used_cells(0):
 				$Highlight.erase_cell(0, n)
+			# for n in _astargrid.get_point_path(source, target):
+			for n in _astar.get_point_path(
+				pack(Vector3i(source.x, source.y, 0), _dim),
+				pack(Vector3i(target.x, target.y, 0), _dim)):
+				$Highlight.set_cell(0, Vector2i(n.x, n.y), 0, Vector2i(0, 0))
+				print("path node: %s (%s) " % [n, pack(n, _dim)])
 			$Highlight.set_cell(0, source, 0, Vector2i(1, 0))
 			$Highlight.set_cell(0, target, 0, Vector2i(2, 0))
-			for n in _astargrid.get_point_path(source, target).slice(1, -1):
-				$Highlight.set_cell(0, $Highlight.local_to_map($Highlight.to_local(n)), 0, Vector2i(0, 0))
 
+func pack(v: Vector3i, dim: Vector3i) -> int:
+	return v.x + v.y * dim.x + v.z * dim.x * dim.y
+
+func unpack(id: int, dim: Vector3i) -> Vector3i:
+	var z = id % (dim.x * dim.y)
+	var y = (id - z) % dim.x
+	var x = (id - z - y)
+	if z < 0 or y < 0 or x < 0:
+		return Vector3i(-1, -1, -1)
+	return Vector3i(x, y, z)	
+	
 func _ready():
 	_astargrid = AStarGrid2D.new()
 	_astargrid.size = $Base.get_used_rect().size
@@ -45,3 +71,29 @@ func _ready():
 			_astargrid.set_point_solid(n)
 		if atlas != Vector2i(2, 0) and atlas != Vector2i(3, 0):
 			_astargrid.set_point_solid(n)
+	
+	_dim = Vector3i(
+		$Base.get_used_rect().size.x,
+		$Base.get_used_rect().size.y,
+		$Base.get_layers_count(),
+	)
+	
+	_astar = AStar3D.new()
+	_astar.reserve_space(_dim.x * _dim.y * _dim.z)
+	for i in range($Base.get_layers_count()):
+		for n in $Base.get_used_cells(i):
+			_astar.add_point(
+				pack(Vector3i(n.x, n.y, i), _dim),
+				Vector3i(n.x, n.y, i))
+	
+	print("_dim: ", _dim)
+	for i in range ($Base.get_layers_count()):
+		for n in $Base.get_used_cells(i):
+			if $Base.get_cell_atlas_coords(i, n) == _ATLAS_GRASS:
+				var source = Vector3i(n.x, n.y, i)
+				for c in $Base.get_surrounding_cells(n):
+					if $Base.get_cell_atlas_coords(i, c) == _ATLAS_GRASS:
+						var target = Vector3i(c.x, c.y, i)
+						print("connecting src: %s (%s) to target: %s (%s)" % [source, pack(source, _dim), target, pack(target, _dim)])
+						_astar.connect_points(pack(source, _dim), pack(target, _dim))
+
--- godot-pathfinding/scenes/world/world.gd
+++ godot-pathfinding/scenes/world/world.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+"""
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -26,3 +26,4 @@ func _physics_process(delta):
 	else:
 		$Map/GI.set_velocity(velocity)
 		$Map/GI.move_and_slide()
+"""
