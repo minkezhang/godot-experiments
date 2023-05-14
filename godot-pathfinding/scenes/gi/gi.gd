20e326504028ec6c08c0deb2bea80ebccac5c7cb
Minke Zhang <minke.zhang@gmail.com>
2023-05-13 20:42:36 -0700

    Refactor gi.dg

--- godot-pathfinding/scenes/gi/gi.gd
+++ godot-pathfinding/scenes/gi/gi.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 120.0
 
+static func map_to_local(map: Vector2i) -> Vector2:
+	return Vector2(map * 64) + Vector2(32, 32)
+
+static func local_to_map(local: Vector2) -> Vector2i:
+	return Vector2i(int(round(local.x)) / 64, int(round(local.y)) / 64)
+
 # Stack of TileMap map coordinates. Stack is in reverse order -- the next
 # waypoint tile is at the end of the data struct.
 var _waypoints: Array[Vector3i]
@@ -14,9 +20,10 @@ func _ready():
 ## Pathfinding cannot interrupt a unit's tile-to-tile movement, and therefore
 ## we will need to report the next "visible" position of the unit.
 func get_waypoint() -> Vector3i:
+	var wp = local_to_map(position)
 	return Vector3i(
-		position.x,
-		position.y,
+		wp.x,
+		wp.y,
 		z_index,
 	) if _waypoints.is_empty() else _waypoints[-1]
 
@@ -27,13 +34,14 @@ func set_waypoint(waypoints: Array[Vector3i]):
 	_waypoints = waypoints
 	_waypoints.reverse()
 
+# Slide to the next waypoint tile.
 func _physics_process(delta):
 	for i in range(_waypoints.size()):
 		var wp = get_waypoint()
-		var dx = Vector2(wp.x, wp.y) - position
-		var v = dx.normalized() * SPEED
+		var wp_local = map_to_local(Vector2i(wp.x, wp.y))
 		
-		set_velocity(v)
+		var dx = wp_local - position
+		var v = dx.normalized() * SPEED if dx.length() > 0 else Vector2(0, 0)
 		
 		if (
 			dx.length() == 0
@@ -41,10 +49,11 @@ func _physics_process(delta):
 			dx.length() < (v * delta).length()
 		):
 			_waypoints.pop_back()
-			
-			position.x = wp.x
-			position.y = wp.y
+			position.x = wp_local.x
+			position.y = wp_local.y
+			set_velocity(Vector2(0, 0))
 		else:
+			set_velocity(v)
 			break
 	
 	move_and_slide()
--- godot-pathfinding/scenes/world/world.gd
+++ godot-pathfinding/scenes/world/world.gd
@@ -8,32 +8,26 @@ func _process(_delta):
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		var mpos = get_global_mouse_position()
-		var source = $Map/Base.local_to_map($Map/Base.to_local(
-			Vector2(
-				$GI.get_waypoint().x,
-				$GI.get_waypoint().y,
-			),
-		))
+		var source = Vector2i(
+			$GI.get_waypoint().x,
+			$GI.get_waypoint().y,
+		)
+		print(source)
 		var target = $Map/Base.local_to_map($Map/Base.to_local(mpos))
 		$Map/Highlight.set_cell(0, target, 0, Vector2i(2, 0))
 	
 		for n in $Map/Highlight.get_used_cells(0):
 			$Map/Highlight.erase_cell(0, n)
+		print("source: %s, target: %s" % [source, target])
 		var path = $Map.astar().get_point_path(
 			$Map/Base.pack(Vector3i(source.x, source.y, 0)),
 			$Map/Base.pack(Vector3i(target.x, target.y, 0)))
-		for n in path:
-			$Map/Highlight.set_cell(0, Vector2i(n.x, n.y), 0, Vector2i(0, 0))
+		for p in path:
+			$Map/Highlight.set_cell(0, Vector2i(p.x, p.y), 0, Vector2i(0, 0))
 		$Map/Highlight.set_cell(0, source, 0, Vector2i(1, 0))
 		$Map/Highlight.set_cell(0, target, 0, Vector2i(2, 0))
 		
-		var _path: Array[Vector3i] = []
+		var waypoints: Array[Vector3i] = []
 		for p in path:
-			_path.append_array(
-				[Vector3i(
-					$Map/Base.map_to_local(Vector2i(p.x, p.y)).x,
-					$Map/Base.map_to_local(Vector2i(p.x, p.y)).y,
-					p.z,
-				)]
-			)
-		$GI.set_waypoint(_path)
+			waypoints.append(Vector3i(p))
+		$GI.set_waypoint(waypoints)
