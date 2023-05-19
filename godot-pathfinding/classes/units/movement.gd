eff0032ea271a957a813a5f0541c5ad8153f1b5b
Minke Zhang <minke.zhang@gmail.com>
2023-05-19 04:42:08 -0700

    Fix map_to_local for pathfinding

--- godot-pathfinding/classes/units/movement.gd
+++ godot-pathfinding/classes/units/movement.gd
@@ -4,4 +4,4 @@ static func map_to_local(map: Vector2i) -> Vector2:
 	return Vector2(map * 64) + Vector2(32, 32)
 
 static func local_to_map(local: Vector2) -> Vector2i:
-	return Vector2i(int(local.x / 64), int(local.y / 64))
+	return Vector2i(floori(local.x / 64), floori(local.y / 64))
